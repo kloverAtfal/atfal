35c049064d03225e48a12a7e1c52421dcdd31ade
iklankerjakosongmalaysia <134831327+devOneGithub@users.noreply.github.com>
2023-07-20 02:37:25 +0800

    v1.0.19

--- functions/index.js
+++ functions/index.js
@@ -15,3 +15,20 @@ document
   .addEventListener('click', function () {
     window.location.href = 'sign-up';
   });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navbar = document.querySelector('.navbar');
+
+  // Function to add or remove the "navbar-scrolled" class based on scroll position
+  function toggleNavbarBlur() {
+    if (window.scrollY > 0) {
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  }
+
+  // Call the function on page load and whenever the user scrolls
+  toggleNavbarBlur();
+  window.addEventListener('scroll', toggleNavbarBlur);
+});
